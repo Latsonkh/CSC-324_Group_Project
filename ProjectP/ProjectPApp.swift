8d9c8dfb59a86528197eb7407d1116f97ae853e8
cbratland <caelan.bratland@gmail.com>
2024-11-04 15:33:10 -0600

    add initial xcodeproj

---
+++ ProjectP/ProjectPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPApp.swift
+//  ProjectP
+//
+//  Created by Caelan on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectPTests/ProjectPTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectPTests.swift
+//  ProjectPTests
+//
+//  Created by Caelan on 11/4/24.
+//
+
+import Testing
+@testable import ProjectP
+
+struct ProjectPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
