23a09f153cd6e3456a521599e52c2ca230fbf7be
cbratland <caelan.bratland@gmail.com>
2024-11-15 15:35:07 -0600

    fix indentation

--- ProjectP/ContentView.swift
+++ ProjectP/ContentView.swift
@@ -32,8 +32,8 @@ struct ContentView: View {
                             let exampleSolution = try await exampleDistanceProblem()
                             // swiftlint:disable line_length
                             self.problem = ProblemInput.text("""
-    A person walks in the following pattern: 3.1 km north, then 2.4 km west, and finally 5.2km south. How far could a bird fly in a straight line from the same starting point to the same final point?
-    """)
+A person walks in the following pattern: 3.1 km north, then 2.4 km west, and finally 5.2km south. How far could a bird fly in a straight line from the same starting point to the same final point?
+""")
                             // swiftlint:enable line_length
                             self.solution = exampleSolution
                             self.showingSolution = true
